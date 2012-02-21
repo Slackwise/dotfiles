029afa4e1ac8daed503b5d6773c44f583d1708d8
Adam Flanczewski <sl@ckwi.se>
2012-02-21 00:56:33 -0600

    Half done with vim/setup.rb

--- vim/setup.rb
+++ vim/setup.rb
@@ -1,13 +1,55 @@
 #!/usr/bin/env ruby
 
-# Ultimately, figure out a way to bootstrap all the below steps with just
-# this Ruby script.
+# This script is supposed to bootstrap my Vim config on a new machine. It
+# should ideally only be run once, with only needing Git pulls to maintain.
 
-# Install/find Git
+CURL = <<CURLEND
+@rem Do not use "echo off" to not affect any child calls.
+@setlocal
 
-# git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+@rem Get the abolute path to the parent directory, which is assumed to be the
+@rem Git installation root.
+@for /F "delims=" %%I in ("%~dp0..") do @set git_install_root=%%~fI
+@set PATH=%git_install_root%\\bin;%git_install_root%\mingw\\bin;%PATH%
 
-# On windows, follow: https://github.com/gmarik/vundle/wiki/Vundle-for-Windows
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
 
-# Add Git\cmd directory to system $PATH
-# Add VIMINIT="so ~/src/dotfiles/vimrc" to my ENV vars.
+@curl.exe %*
+CURLEND
+
+def windows_setup
+  # Write curl.cmd to Git/cmd directory.
+  # Add Git/cmd directory to system $PATH.
+end
+
+def nixie_setup
+  # Symlink vimrc, nothing more!
+end
+
+def is_admin?
+  if ARGV.include? "-a"
+    true
+  else
+    false
+  end
+end
+
+def restart_as_admin
+  require 'win32ole'
+  WIN32OLE.new('Shell.Application')
+    .ShellExecute('ruby.exe', File::expand_path(__FILE__) + " -a",
+                  nil, 'runas', 1)
+end
+
+case RUBY_PLATFORM
+when /linux/, /darwin/
+  nixie_setup
+when /mswin/, /mingw32/
+  puts ENV['USERNAME']
+  if is_admin?
+    windows_setup
+  else
+    restart_as_admin
+  end
+end
