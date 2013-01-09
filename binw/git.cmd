f08b475e2e5901f1c8367bf1cb1a2f062bcef7a7
Adam Flanczewski <sl@ckwi.se>
2013-01-09 13:27:13 -0600

    Added curl to binw.

--- binw/git.cmd
+++ binw/git.cmd
@@ -2,7 +2,7 @@
 
 set github_dir=%LOCALAPPDATA%\GitHub
 
-@rem Finds the newest version of PortableGit instlled by GitHub.
+@rem Finds the newest version of PortableGit installed by GitHub.
 for /F "tokens=*" %%f in ('dir /O:D /B %github_dir%\PortableGit_*') do set git=%%f
 
 %github_dir%\%git%\cmd\git.exe %*
