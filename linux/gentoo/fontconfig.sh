64e5adb8d9df62ba5799111849d8f9f0d00b330b
Adam Flanczewski <sl@ckwi.se>
2013-05-27 21:34:50 -0500

    Updated fontconfig.

--- linux/gentoo/fontconfig.sh
+++ linux/gentoo/fontconfig.sh
@@ -2,4 +2,4 @@ echo "media-libs/freetype infinality" >> /etc/portage/package.use
 emerge freetype
 eselect fontconfig enable 52-infinality.conf
 eselect infinality set osx2
-eselect lcdfilter set osx
+eselect lcdfilter set default
