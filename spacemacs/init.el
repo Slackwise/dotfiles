876525c03f47b9ad809f414f038d5809997391bc
Adam Flanczewski <sl@ckwi.se>
2015-10-16 14:10:55 -0500

    Set Spacemacs theme to 'spacemacs-dark'.

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -37,7 +37,6 @@ values."
      ;; version-control
      ruby
      clojure
-     ;; twittering-mode ;; Need to find a proper layer
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -81,15 +80,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(material
-                         spacemacs-dark
-                         spacemacs-light
-                         solarized-light
-                         solarized-dark
-                         leuven
-                         monokai
-                         tango
-                         zenburn)
+   dotspacemacs-themes '(spacemacs-dark)
    ;; If non nil the cursor color matches the state color.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
@@ -197,6 +188,8 @@ values."
   "Initialization function for user code.
 It is called immediately after `dotspacemacs/init'.  You are free to put any
 user code."
+    (setq custom-file "~/.spacemacs-custom.el")
+    (load custom-file)
   )
 
 (defun dotspacemacs/user-config ()
