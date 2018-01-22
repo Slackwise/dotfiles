2e6addba618526b3fedff7dd323181b523147949
Adam Flanczewski <sl@ckwi.se>
2018-01-22 00:05:01 -0600

    CSS: Added defined dotfiles dir.

--- css/Setup (Run as Admin).bat
+++ css/Setup (Run as Admin).bat
@@ -1,7 +1,8 @@
 set CSS_CFG_DIR="C:\Program Files (x86)\Steam\userdata\19911248\240\remote\cfg"
+set DOTFILES_DIR="C:\Users\Slackwise\src\dotfiles\css"
 
-copy /y %CSS_CFG_DIR%\config.cfg . &&^
+copy /y %CSS_CFG_DIR%\config.cfg %DOTFILES_DIR% &&^
  rmdir /s /q %CSS_CFG_DIR &&^
- mklink /d %CSS_CFG_DIR% .
+ mklink /d %CSS_CFG_DIR% %DOTFILES_DIR%
 
 pause
