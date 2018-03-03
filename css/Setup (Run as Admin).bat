0bcf46a14fce94e9e7b0f70120009173a9aa5958
Adam Flanczewski <sl@ckwi.se>
2018-03-02 18:42:28 -0600

    Updated spacemacs Rakefile
    Also updated CS Source script, minor.

--- css/Setup (Run as Admin).bat
+++ css/Setup (Run as Admin).bat
@@ -1,7 +1,7 @@
 set CSS_CFG_DIR="C:\Program Files (x86)\Steam\userdata\19911248\240\remote\cfg"
 set DOTFILES_DIR="C:\Users\Slackwise\src\dotfiles\css"
 
-copy /y %CSS_CFG_DIR%\config.cfg %DOTFILES_DIR% &&^
+copy /y %CSS_CFG_DIR%\* %DOTFILES_DIR% &&^
  rmdir /s /q %CSS_CFG_DIR &&^
  mklink /d %CSS_CFG_DIR% %DOTFILES_DIR%
 
