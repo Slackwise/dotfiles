1d9752609fd6268e157da85be23d726407cae07a
Adam Flanczewski <sl@ckwi.se>
2018-04-21 09:31:46 -0500

    Measure-LongPaths: Updated param logic.

--- binw/Measure-LongPaths.ps1
+++ binw/Measure-LongPaths.ps1
@@ -1,19 +1,14 @@
 # vim: set ts=4 sw=4:
 
-
-# Config Vars
-$ROOT_PATH      = "/home/slackwise"
-$LENGTH_TO_LOG  = 256
-
-
-
-
-
-function Measure-LongPaths($path=$ROOT_PATH) {
+function Measure-LongPaths {
+    param (
+        [int]$Size = 256,
+        [parameter(Position=0)]$Path = $pwd
+    )
     begin
     {
-        $longPaths = New-Object System.Collections.Generic.List[System.Object]
-        $logPathFn = { if ($_.FullName.Length -ge $LENGTH_TO_LOG) { $longpaths.Add($_.FullName) } }
+        $longPaths = New-Object System.Collections.ArrayList
+        $logPathFn = { if ($_.FullName.Length -ge $LENGTH_TO_LOG) {$longpaths.Add($_.FullName)} }
     }
     process
     {
