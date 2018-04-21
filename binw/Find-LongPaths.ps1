5ed7f0e16d711cf09fe6c9042c67b1adb8934e47
Adam Flanczewski <sl@ckwi.se>
2018-04-21 10:18:51 -0500

    Find-LongPaths: Renamed from Measure-LongPaths, and more:
    - Added comment-based help text.
    - Added type attributes.
    - Added ability to run as script directly. (Maybe not idiomatically.)

---
+++ binw/Find-LongPaths.ps1
@@ -0,0 +1,49 @@
+# vim: set ts=4 sw=4:
+
+<#
+.SYNOPSIS
+Returns list of full paths that exceed 256 chars or -Length
+
+.DESCRIPTION
+Recursively traverses a given -Path (default: current directory) and returns
+a list of any full paths that exceed -Length (default: 256) characters.
+
+.PARAMETER Length
+Character length of full path that will be logged
+
+.PARAMETER Path
+The path to recursively traverse to find long paths
+
+.INPUTS
+Integer Length
+String Path
+
+.OUTPUTS
+List<String>
+
+.LINK
+Source for the script: https://github.com/Slackwise/dotfiles/blob/master/binw/Find-LongPaths.ps1
+#>
+function Find-LongPaths {
+    param (
+        [int]$Length = 256,
+        [parameter(Position=0)][String]$Path = $pwd
+    )
+    begin
+    {
+        $longPaths = New-Object System.Collections.Generic.List[String]
+        $logPathFn = { if ($_.FullName.Length -ge $Length) {$longPaths.Add($_.FullName)} }
+    }
+    process
+    {
+        dir $path -Recurse | % $logPathFn
+    }
+    end
+    {
+        # Normally, I'd wish to instead return some sort of object representing
+        # paths, but .NET does not provide one, so an Array of Strings it is!
+        return $longPaths.ToArray()
+    }
+}
+
+if ($args) { Find-LongPaths $args[0] }
