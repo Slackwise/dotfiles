229147189b12cd7e49eb2360117206cbcb0aefdc
Adam Flanczewski <sl@ckwi.se>
2018-04-21 23:36:20 -0500

    CS:Source: Setup config script!

--- css/setup.bat
+++ css/setup.bat
@@ -1,6 +1,6 @@
 @echo off
 set CONFIG_FILE="slackwise.cfg"
-set CSS_DIR="C:\Program Files (x86)\Steam\userdata\19911248\240\remote\cfg"
+set CSS_DIR="C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Source\cstrike\cfg"
 set DOTFILES_DIR="C:\Users\Slackwise\src\dotfiles\css"
 set CSS_FILE=%CSS_DIR%\%CONFIG_FILE%
 set DOTFILE=%DOTFILES_DIR%\%CONFIG_FILE%
