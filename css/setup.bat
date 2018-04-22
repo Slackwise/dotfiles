3b80648b010bb72672ffb772bbcf04568718a902
Adam Flanczewski <sl@ckwi.se>
2018-04-22 13:44:42 -0500

    CS:Source: Added buy scripts!
    Need to test the buypreset scripts, tho.

--- css/setup.bat
+++ css/setup.bat
@@ -1,11 +1,22 @@
 @echo off
-set CONFIG_FILE="slackwise.cfg"
 set CSS_DIR="C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Source\cstrike\cfg"
 set DOTFILES_DIR="C:\Users\Slackwise\src\dotfiles\css"
-set CSS_FILE=%CSS_DIR%\%CONFIG_FILE%
-set DOTFILE=%DOTFILES_DIR%\%CONFIG_FILE%
+set CONFIG_FILE="slackwise.cfg"
+set AUTOBUY_FILE="autobuy.txt"
+set CT_BUY_FILE="buypresets_ct.vdf"
+set T_BUY_FILE="buypresets_ter.vdf"
 
-del %CSS_FILE%
-mklink /h %CSS_FILE% %DOTFILE%
+del %CSS_DIR%\%CONFIG_FILE% 
+del %CSS_DIR%\%AUTOBUY_FILE%
+del %CSS_DIR%\%CT_BUY_FILE% 
+del %CSS_DIR%\%T_BUY_FILE%  
 
+mklink /h %CSS_DIR%\%CONFIG_FILE%          %DOTFILES_DIR%\%CONFIG_FILE%
+mklink /h %CSS_DIR%\%AUTOBUY_FILE%         %DOTFILES_DIR%\%AUTOBUY_FILE%
+mklink /h %CSS_DIR%\%CT_BUY_FILE%          %DOTFILES_DIR%\%CT_BUY_FILE%
+mklink /h %CSS_DIR%\%T_BUY_FILE%           %DOTFILES_DIR%\%T_BUY_FILE% 
+
+echo "=============================================="
+echo "=== Make sure to set your LAUNCH OPTIONS!" ==="
+echo "=============================================="
 pause
