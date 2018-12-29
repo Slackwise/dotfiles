cda98c661fd1e50d1e776c27291949a7dc1f7d82
Adam Flanczewski <sl@ckwi.se>
2018-12-29 00:37:02 -0600

    evil-mswin.el: Nuked mswin.vim commentary. Rewrote paste functions.
    I removed all the original commentary since it scattered all the associated
    functionality across the file. Now all the paste mappings are grouped together.
    
    I also rewrote paste in terms of evil functions so user mapping changes won't
    impact its behavior. (i.e. When a user changes `C-v` it won't incidentally call
    that new macro instead of paste.)

--- spacemacs/evil-mswin.el
+++ spacemacs/evil-mswin.el
@@ -1,170 +1,59 @@
+;;;  -*- lexical-binding: t; -*-
 ;;; Approximate mswin.vim behavior for Emacs evil-mode.
+;; Ported the most relevant keybindings from https://github.com/vim/vim/blob/master/runtime/mswin.vim
 
-;; I will attempt to rewrite the relevant statements line-by-line from:
-;; https://github.com/vim/vim/blob/master/runtime/mswin.vim
 
-(setq lexical-binding t)
-
-;; > " Bail out if this isn't wanted.
-;; > if exists("g:skip_loading_mswin") && g:skip_loading_mswin
-;; >   finish
-;; > endif
-;; #NOTE: No, we do want this.
-
-;; > " set the 'cpoptions' to its Vim default
-;; > if 1	" only do this when compiled with expression evaluation
-;; >   let s:save_cpo = &cpoptions
-;; > endif
-;; > set cpo&vim
-;; #NOTE: This backs up the options which get restored at the end of the script,
-;; but there is no analog for this in evil-mode so it doesn't matter.
-
-;; > " set 'selection', 'selectmode', 'mousemodel' and 'keymodel' for MS-Windows
-;; > behave mswin
-;; #TODO: Figure out what mouse options are even possible to port.
-
-;; > " backspace and cursor keys wrap to previous/next line
-;; > set backspace=indent,eol,start whichwrap+=<,>,[,]
-;; #NOTE: Emacs already behaves this way by default! Nothing to do here.
-
-;; > " backspace in Visual mode deletes selection
-;; > vnoremap <BS> d
+;; Delete in visual mode
 (define-key evil-visual-state-map (kbd "DEL") 'evil-delete)
 
-;; > if has("clipboard")
-;; >     " CTRL-X and SHIFT-Del are Cut
-;; >     vnoremap <C-X> "+x
-;; >     vnoremap <S-Del> "+x
+;; Cut
 (define-key evil-visual-state-map "\C-x" "\"+x")
 (define-key evil-visual-state-map (kbd "S-DEL") "\"+x")
 
-;; >     " CTRL-C and CTRL-Insert are Copy
-;; >     vnoremap <C-C> "+y
-;; >     vnoremap <C-Insert> "+y
+;; Copy
 (let ((yank (lambda ()
               (interactive)
               (apply 'evil-yank (append (seq-take (evil-visual-range) 3) '(?+))))))
   (define-key evil-visual-state-map "\C-c" yank)
   (define-key evil-visual-state-map [C-insert] yank))
 
-;; >     " CTRL-V and SHIFT-Insert are Paste
-;; >     map <C-V>		"+gP
-;; >     map <S-Insert>		"+gP
-(define-key evil-visual-state-map "\C-v" "\"+P") ; #NOTE: gP doesn't exist in evil
-(define-key evil-visual-state-map [S-insert] "\"+p")
-
-;; >     cmap <C-V>		<C-R>+
-;; >     cmap <S-Insert>		<C-R>+
-(define-key evil-command-window-mode-map "\C-v" (kbd "C-r +"))
-(define-key evil-command-window-mode-map [S-insert] (kbd "C-r +"))
-;; > endif
-;; #TODO: Use evil-paste-from-register directly?
-;; #NOTE: All the above commands use the * register because + apparently doesn't work. (Windows only...?)
-
-;; > " Pasting blockwise and linewise selections is not possible in Insert and
-;; > " Visual mode without the +virtualedit feature.  They are pasted as if they
-;; > " were characterwise instead.
-;; > " Uses the paste.vim autoload script.
-;; > " Use CTRL-G u to have CTRL-Z only undo the paste.
-;; > if 1
-;; >     exe 'inoremap <script> <C-V> <C-G>u' . paste#paste_cmd['i']
-;; >     exe 'vnoremap <script> <C-V> ' . paste#paste_cmd['v']
-;; > endif
-;; #NOTE: I don't know what this even does, but I presume evil does better out of the box.
-
-;; >;; > imap <S-Insert>		<C-V>
-;; > vmap <S-Insert>		<C-V>
-(define-key evil-insert-state-map "\C-v" (kbd "C-r +"))
-(define-key evil-insert-state-map [S-insert] (kbd "C-r +"))
-
-;; > " Use CTRL-Q to do what CTRL-V used to do
-;; > noremap <C-Q>		<C-V>
-(define-key evil-normal-state-map "\C-q" 'evil-visual-block)
-
-;; > " Use CTRL-S for saving, also in Insert mode (<C-O> doesn't work well when
-;; > " using completions).
-;; > noremap <C-S>		:update<CR>
-;; > vnoremap <C-S>		<C-C>:update<CR>
-;; > inoremap <C-S>		<Esc>:update<CR>gi
+;; Paste
+(let ((paste (lambda ()
+               (interactive)
+               (evil-paste-from-register ?+)))
+      ;; (paste-before (lambda (count &optional register yank-handler)
+      ;;                 (interactive "P<x>")
+      (paste-before (lambda (count &optional register yank-handler)
+                      (interactive "P<x>")
+                      (call-interactively 'evil-paste-before)))
+      (paste-after (lambda (count &optional register yank-handler)
+                      (interactive "P<x>")
+                      (call-interactively 'evil-paste-after))))
+  (define-key evil-command-window-mode-map "\C-v" paste)
+  (define-key evil-command-window-mode-map [S-insert] paste)
+  (define-key evil-insert-state-map "\C-v" paste)
+  (define-key evil-insert-state-map [S-insert] paste)
+  (define-key evil-visual-state-map "\C-v" paste-before)
+  (define-key evil-visual-state-map [S-insert] paste-after)
+  (define-key evil-normal-state-map "\C-v" paste-before)
+  (define-key evil-normal-state-map [S-insert] paste-after))
+ 
+;; Save
 (define-key evil-normal-state-map "\C-s" 'evil-write)
 (define-key evil-visual-state-map "\C-s" 'evil-write)
 (define-key evil-insert-state-map "\C-s" 'evil-write)
 
-;; > " For CTRL-V to work autoselect must be off.
-;; > " On Unix we have two selections, autoselect can be used.
-;; > if !has("unix")
-;; >   set guioptions-=a
-;; > endif
-;; #NOTE: The + register doesn't even work so I doubt this is relevant.
-
-;; > " CTRL-Z is Undo; not in cmdline though
-;; > noremap <C-Z> u
-;; > inoremap <C-Z> <C-O>u
+;; Undo
 (define-key evil-normal-state-map "\C-z" 'undo-tree-undo)
 (define-key evil-insert-state-map "\C-z" 'undo-tree-undo)
-;; #NOTE: Despite not having an evil- prefix, it looks like undo-tree- is by the evil devs?
 
-;; > " CTRL-Y is Redo (although not repeat); not in cmdline though
-;; > noremap <C-Y> <C-R>
- ;; > inoremap <C-Y> <C-O><C-R>
-;; #NOTE: I don't think this is muscle memory for anyone, but if it is, I'll add it in.
-
-;; > " Alt-Space is System menu
-;; > if has("gui")
-;; >   noremap <M-Space> :simalt ~<CR>
-;; >   inoremap <M-Space> <C-O>:simalt ~<CR>
-;; >   cnoremap <M-Space> <C-C>:simalt ~<CR>
-;; > endif
-;; #NOTE: This would be an okay default, if this script got loaded before users other configs.
-
-;; > " CTRL-A is Select all
-;; > noremap <C-A> gggH<C-O>G
-;; > inoremap <C-A> <C-O>gg<C-O>gH<C-O>G
-;; > cnoremap <C-A> <C-C>gggH<C-O>G
-;; > onoremap <C-A> <C-C>gggH<C-O>G
-;; > snoremap <C-A> <C-C>gggH<C-O>G
-;; > xnoremap <C-A> <C-C>ggVG
+;; Select All
 (let ((evil-select-all (lambda ()
                          (interactive)
                          (evil-visual-select 0 (point-max)))))
   (define-key evil-normal-state-map "\C-a" evil-select-all)
   (define-key evil-insert-state-map "\C-a" evil-select-all)
   (define-key evil-command-window-mode-map "\C-a" evil-select-all))
-;; #NOTE: The rest of the modes have no maps.
 
-;; > " CTRL-Tab is Next window
-;; > noremap <C-Tab> <C-W>w
-;; > inoremap <C-Tab> <C-O><C-W>w
-;; > cnoremap <C-Tab> <C-C><C-W>w
-;; > onoremap <C-Tab> <C-C><C-W>w
-;; #NOTE: This would probably just break people's workflow, especially in Spacemacs.
-
-;; > " CTRL-F4 is Close window
-;; > noremap <C-F4> <C-W>c
-;; > inoremap <C-F4> <C-O><C-W>c
-;; > cnoremap <C-F4> <C-C><C-W>c
-;; > onoremap <C-F4> <C-C><C-W>c
-;; #NOTE: Another UI related binding that might break workflow, especially in Spacemacs.
-
-;; > if has("gui")
-;; >   " CTRL-F is the search dialog
-;; >   noremap  <expr> <C-F> has("gui_running") ? ":promptfind\<CR>" : "/"
-;; >   inoremap <expr> <C-F> has("gui_running") ? "\<C-\>\<C-O>:promptfind\<CR>" : "\<C-\>\<C-O>/"
-;; >   cnoremap <expr> <C-F> has("gui_running") ? "\<C-\>\<C-C>:promptfind\<CR>" : "\<C-\>\<C-O>/"
-
-;; >   " CTRL-H is the replace dialog,
-;; >   " but in console, it might be backspace, so don't map it there
-;; >   nnoremap <expr> <C-H> has("gui_running") ? ":promptrepl\<CR>" : "\<C-H>"
-;; >   inoremap <expr> <C-H> has("gui_running") ? "\<C-\>\<C-O>:promptrepl\<CR>" : "\<C-H>"
-;; >   cnoremap <expr> <C-H> has("gui_running") ? "\<C-\>\<C-C>:promptrepl\<CR>" : "\<C-H>"
-;; > endif
-;; #TODO: Not sure yet what the analog for these would be in evil-mode.
-
-;; > " restore 'cpoptions'
-;; > set cpo&
-;; > if 1
-;; >   let &cpoptions = s:save_cpo
-;; >   unlet s:save_cpo
-;; > endif
-;; #NOTE: Well we never touched these options at the top of the file,
-;; so there's nothing to set back.
+;; Visual block select
+(define-key evil-normal-state-map "\C-q" 'evil-visual-block)
