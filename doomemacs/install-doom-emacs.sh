5cce5bfeb3602ad0e353ec4968f78c3d76292d32
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-10-10 14:37:49 -0500

    Corrected doom emacs install script.

--- doomemacs/install-doom-emacs.sh
+++ doomemacs/install-doom-emacs.sh
@@ -8,15 +8,20 @@ emacs_config_dir=~/.emacs.d
 # Clone doom repo and symlink it to the emacs directory:
 if [[ ! -e $doom_repo_dir ]]; then
   git clone --depth 1 $doom_repo_url $doom_repo_dir
-  if [[ ! -e $emacs_config_dir ]]; then
-    ln -s $doom_repo_dir $emacs_config_dir
-  fi
+fi
+
+# Symlink doom repo to emacs directory:
+if [[ ! -e $emacs_config_dir ]]; then
+  ln -s $doom_repo_dir $emacs_config_dir
 fi
 
 # Symlink my dotfiles config to Doom's config directory:
 if [[ ! -L $doom_config_dir ]]; then
   ln -s $dotfile_config_dir $doom_config_dir
 fi
+
+# Reload bash config so it can find the doom cmmands:
+source ~/.bashrc
 
 # Make Doom fetch and build its elisp packages:
 if [[ -x "$(command -v doom)" ]]; then
