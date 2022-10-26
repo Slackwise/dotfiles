b4746913ee43ed63f236c8cdb75f6b1d5f666ed7
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-10-25 21:19:58 -0500

    git-config: Migrated Rakefile to shell script

---
+++ git/git-config.sh
@@ -0,0 +1,11 @@
+git config --global user.name "Adam Flanczewski"
+git config --global user.email "76595+Slackwise@users.noreply.github.com"
+git config --global core.eol lf
+git config --global core.autocrlf false
+git config --global core.editor vim
+git config --global color.ui always
+git config --global diff.tool vimdiff
+git config --global diff.merge vimdiff
+git config --global difftool.prompt false
+git config --global alias.c "commit -am"
+git config --global alias.p "push"
