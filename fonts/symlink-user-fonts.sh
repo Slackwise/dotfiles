473c46284fce441346409083787b0a107369245d
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-12-21 15:26:49 -0600

    Fixed fonts/symlink-user-fonts.sh

--- fonts/symlink-user-fonts.sh
+++ fonts/symlink-user-fonts.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+user_font_dir=$HOME/.local/share/fonts
+
+mkdir -p $user_font_dir
+
 ln -s \
-  $HOME/src/dotfiles/fonts \
-  $HOME/.local/share/fonts/dotfiles-fonts
+  "$HOME/src/dotfiles/fonts" \
+  "$user_font_dir/dotfiles-fonts"
