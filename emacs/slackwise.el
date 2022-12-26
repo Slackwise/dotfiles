8f39645b09fab5b57e47357f653646281785edc6
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-12-26 11:24:53 -0600

    slackwise.el: Updated and added org/roam templates

--- emacs/slackwise.el
+++ emacs/slackwise.el
@@ -3,12 +3,18 @@
 
 ;;; slackwise --- Adam "Slackwise" Flanczewski's Emacs config
 
+
+;; Author: Adam "Slackwise" Flanczewski
+;; Package-Requires: ((emacs "24.1") (s "1.13.1"))
+;; URL: https://github.com/Slackwise/dotfiles/
+
 ;;; Commentary:
-;;; My personal config tweaks designed to be used as a stand-alone
-;;; configuration or for Doom/Spacemacs.
+;; My personal config tweaks designed to be used as a stand-alone
+;; configuration or for Doom/Spacemacs.
 
 ;;; Code:
 (package-initialize)
+(require 's)
 
 ; (load-theme 'vscode-dark-plus) ; loaded via doom's config
 
@@ -27,13 +33,34 @@
 ;; Disable toolbar:
 (tool-bar-mode -1) ; still accessible via F10
 
+(defun concat-lines (&rest lines)
+  "Concatenate strings &LINES into one newline-seperated string."
+  (s-join "\n" lines))
+  ;(apply 'string-join (cons lines "\n"))
+  ;(seq-reduce (lambda (line s) (concat line s)) lines ""))
+
 ;; Org/Roam:
 (setq org-directory (file-truename "~/notes"))
 (after! org (setq
              org-roam-directory (file-truename "~/notes")
              org-roam-capture-templates '(("d" "default" plain "%?"
+                                           :target (file+head "private/captures/${slug}.org"
+                                                              (concat-lines "#+title: ${title}"
+                                                                            "#+filetags: private captures"))
+                                           :unnarrowed t)
+                                          ("p" "private" plain "%?"
+                                           :target (file+head "private/${slug}.org"
+                                                              (concat-lines "#+title: ${title}"
+                                                                            "#+filetags: private"))
+                                           :unnarrowed t)
+                                          ("w" "work" plain "%?"
+                                           :target (file+head "private/work/${slug}.org"
+                                                              (concat-lines "#+title: ${title}"
+                                                                            "#+filetags: private work"))
+                                           :unnarrowed t)
+                                          ("n" "public" plain "%?"
                                            :target (file+head "${slug}.org"
-                                                              "#+title: ${title}\n")
+                                                              "#+title: ${title}")
                                            :unnarrowed t))))
 ; (org-roam-db-autosync-mode) ; unnecessary?
 
@@ -47,7 +74,7 @@
   (add-hook 'ruby-mode-hook #'(lambda () (modify-syntax-entry ?_ "w")))
   (add-hook 'python-mode-hook #'(lambda () (modify-syntax-entry ?_ "w")))
 
-;; Configure Evil mode.
+;; Configure Evil mode:
 (load "~/src/dotfiles/emacs/evil-mswin.el")
 ;; (load "~/src/dotfiles/emacs/evil-config.el") ; Do I really need to separate this?
 
