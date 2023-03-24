d51b5ebbd3d15879b80123da9a082449e23e86f2
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2023-03-23 23:55:16 -0500

    Emacs: org-roam filename underscores to dashes

--- emacs/slackwise.el
+++ emacs/slackwise.el
@@ -13,6 +13,7 @@
 ;; configuration or for Doom/Spacemacs.
 
 ;;; Code:
+(message "<<< SLACKWISE.EL STARTED >>>")
 (package-initialize)
 (require 's)
 
@@ -75,47 +76,11 @@
 ;; Org/Roam:
 (setq org-directory (file-truename "~/notes"))
 (setq emacsql-sqlite-executable "~/.guix-profile/bin/emacsql-sqlite")
+(advice-add 'org-roam-node-slug :filter-return
+            (lambda (node-name)
+              (s-replace "_" "-" node-name)))
+
 (after! org
-  ;; Override how org-roam generates filenames/slugs, replacing its underscores with hyphens/dashes:
-  ;; (Solution found on Reddit: https://www.reddit.com/r/emacs/comments/omxl6n/config_for_orgroam_v2/ )
-  (cl-defmethod org-roam-node-slug ((node org-roam-node))
-    "Return the slug of NODE."
-    (let ((title (org-roam-node-title node))
-          (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-                             768 ; U+0300 COMBINING GRAVE ACCENT
-                             769 ; U+0301 COMBINING ACUTE ACCENT
-                             770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
-                             771 ; U+0303 COMBINING TILDE
-                             772 ; U+0304 COMBINING MACRON
-                             774 ; U+0306 COMBINING BREVE
-                             775 ; U+0307 COMBINING DOT ABOVE
-                             776 ; U+0308 COMBINING DIAERESIS
-                             777 ; U+0309 COMBINING HOOK ABOVE
-                             778 ; U+030A COMBINING RING ABOVE
-                             779 ; U+030B COMBINING DOUBLE ACUTE ACCENT
-                             780 ; U+030C COMBINING CARON
-                             795 ; U+031B COMBINING HORN
-                             803 ; U+0323 COMBINING DOT BELOW
-                             804 ; U+0324 COMBINING DIAERESIS BELOW
-                             805 ; U+0325 COMBINING RING BELOW
-                             807 ; U+0327 COMBINING CEDILLA
-                             813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-                             814 ; U+032E COMBINING BREVE BELOW
-                             816 ; U+0330 COMBINING TILDE BELOW
-                             817 ; U+0331 COMBINING MACRON BELOW
-                             )))
-      (cl-flet* ((nonspacing-mark-p (char) (memq char slug-trim-chars))
-                 (strip-nonspacing-marks (s) (string-glyph-compose
-                                              (apply #'string
-                                                     (seq-remove #'nonspacing-mark-p
-                                                                 (string-glyph-decompose s)))))
-                 (cl-replace (title pair) (replace-regexp-in-string (car pair) (cdr pair) title)))
-        (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-") ;; convert anything not alphanumeric
-                        ("__*" . "-")                   ;; remove sequential dashes
-                        ("^_" . "")                     ;; remove starting dash
-                        ("_$" . "")))                   ;; remove ending dash
-               (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
-          (downcase slug)))))
   (setq org-roam-directory (file-truename "~/notes")
         org-roam-capture-templates `(("d" "default" plain "%?"
                                       :target (file+head "private/captures/${slug}.org"
@@ -137,8 +102,50 @@
                                                          ,(concat-lines "#+title: ${title}"
                                                                         "#+filetags: "))
                                       :unnarrowed t))))
-                                        ; (org-roam-db-autosync-mode) ; unnecessary?
 
+;; (after! org-roam
+;;   ;; Override how org-roam generates filenames/slugs, replacing its underscores with hyphens/dashes:
+;;   ;; (Solution found on Reddit: https://www.reddit.com/r/emacs/comments/omxl6n/config_for_orgroam_v2/ )
+;;   (cl-defmethod org-roam-node-slug ((node org-roam-node))
+;;     "Return a (CUSTOM) slug of NODE."
+;;     (let ((title (org-roam-node-title node))
+;;           (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+;;                              768 ; U+0300 COMBINING GRAVE ACCENT
+;;                              769 ; U+0301 COMBINING ACUTE ACCENT
+;;                              770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
+;;                              771 ; U+0303 COMBINING TILDE
+;;                              772 ; U+0304 COMBINING MACRON
+;;                              774 ; U+0306 COMBINING BREVE
+;;                              775 ; U+0307 COMBINING DOT ABOVE
+;;                              776 ; U+0308 COMBINING DIAERESIS
+;;                              777 ; U+0309 COMBINING HOOK ABOVE
+;;                              778 ; U+030A COMBINING RING ABOVE
+;;                              779 ; U+030B COMBINING DOUBLE ACUTE ACCENT
+;;                              780 ; U+030C COMBINING CARON
+;;                              795 ; U+031B COMBINING HORN
+;;                              803 ; U+0323 COMBINING DOT BELOW
+;;                              804 ; U+0324 COMBINING DIAERESIS BELOW
+;;                              805 ; U+0325 COMBINING RING BELOW
+;;                              807 ; U+0327 COMBINING CEDILLA
+;;                              813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+;;                              814 ; U+032E COMBINING BREVE BELOW
+;;                              816 ; U+0330 COMBINING TILDE BELOW
+;;                              817 ; U+0331 COMBINING MACRON BELOW
+;;                              )))
+;;       (cl-flet* ((nonspacing-mark-p (char) (memq char slug-trim-chars))
+;;                  (strip-nonspacing-marks (s) (string-glyph-compose
+;;                                               (apply #'string
+;;                                                      (seq-remove #'nonspacing-mark-p
+;;                                                                  (string-glyph-decompose s)))))
+;;                  (cl-replace (title pair) (replace-regexp-in-string (car pair) (cdr pair) title)))
+;;         (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-") ;; convert anything not alphanumeric
+;;                         ("__*" . "-")                   ;; remove sequential dashes
+;;                         ("^_" . "")                     ;; remove starting dash
+;;                         ("_$" . "")))                   ;; remove ending dash
+;;                (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
+;;           (downcase slug)))))
+;;   )
 
 (provide 'slackwise)
+(message "<<< SLACKWISE.EL LOADED >>>")
 ;;; slackwise.el ends here
