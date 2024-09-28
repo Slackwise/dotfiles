7ca722839d5ca02ad088c877fda4263c994537dd
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2024-09-27 20:05:27 -0500

    Windows Scripts: added

---
+++ windows/bin/toggle-theme.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal
+
+REM Registry keys for light/dark mode
+set key=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize
+set light_theme=1
+set dark_theme=0
+
+REM Get the current AppsUseLightTheme value
+for /f "tokens=3" %%A in ('reg query "%key%" /v AppsUseLightTheme') do set current_mode=%%A
+
+if "%current_mode%"=="0x1" (
+    REM Currently in light mode, switch to dark mode
+    reg add "%key%" /v AppsUseLightTheme /t REG_DWORD /d %dark_theme% /f
+    REM reg add "%key%" /v SystemUsesLightTheme /t REG_DWORD /d %dark_theme% /f
+    echo Switched to dark mode.
+) else (
+    REM Currently in dark mode, switch to light mode
+    reg add "%key%" /v AppsUseLightTheme /t REG_DWORD /d %light_theme% /f
+    REM reg add "%key%" /v SystemUsesLightTheme /t REG_DWORD /d %light_theme% /f
+    echo Switched to light mode.
+)
+
+REM Force an update by refreshing the theme
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters
+
+endlocal
